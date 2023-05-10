57713151b5789a17ffcaabdf4470a19b18836340
sophiahoelzl <sophia.hoelzl@stud.fh-campuswien.ac.at>
2023-05-10 22:15:10 +0200

    Profile Screen finished

--- app/src/main/java/com/example/procrasticure/Widgets/AppBar.kt
+++ app/src/main/java/com/example/procrasticure/Widgets/AppBar.kt
@@ -1,10 +1,7 @@
 package com.example.procrasticure.Widgets
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
@@ -14,7 +11,9 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.procrasticure.Screens.Screen
 
@@ -23,13 +22,15 @@ fun TopMenu(navController: NavController){
     var showMenu by remember { mutableStateOf(false) }
     androidx.compose.material.TopAppBar(elevation = 2.dp) {
         Row() {
-            Text(text = "ProcrastiCure")
-            Spacer(modifier = Modifier.width(275.dp))
+            Text(text = "ProcrastiCure", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.weight(1f))
             Box(contentAlignment = Alignment.TopEnd) {
                 Icon(
                     imageVector = Icons.Default.Menu,
                     contentDescription = "menu",
-                    modifier = Modifier.clickable { showMenu = !showMenu })
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clickable { showMenu = !showMenu })
                 DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
                     DropdownMenuItem(onClick = { navController.navigate(Screen.ProfileScreen.route) }) {
                         Text(text = "Profile")
