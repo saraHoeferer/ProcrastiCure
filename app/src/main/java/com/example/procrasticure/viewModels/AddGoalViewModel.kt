175201f6046199ca33e2b41589f39453f39f86cd
sophiahoelzl <sophia.hoelzl@stud.fh-campuswien.ac.at>
2023-05-22 15:39:36 +0200

    added all ViewModels

---
+++ app/src/main/java/com/example/procrasticure/viewModels/AddGoalViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.procrasticure.viewModels
+
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
+class AddGoalViewModel() : ViewModel() {
+
+    val db = Firebase.firestore
+
+    fun onSubmitClicked(){
+
+    }
+
+
+
+
+}
